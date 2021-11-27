245d816aecbeebc68c5ac60d3fe799490b9837ca
Heelio <Heelio@users.noreply.github.com>
2021-11-27 22:55:01 +0100

    Initial commit

---
+++ Heelio.EotEBrighterDreams/EotEBrighterDreams.cs
@@ -0,0 +1,78 @@
+﻿using OWML.ModHelper;
+using OWML.Common;
+using UnityEngine;
+
+namespace EotEBrighterDreams
+{
+    public class EotEBrighterDreams : ModBehaviour
+    {
+        DreamLanternController _dreamLanternController;
+        Light _light;
+        bool _isLoaded;
+
+        float _lightRange = 10f;
+        float _lightIntensity = .1f;
+        bool _enabled = true;
+
+        void Start()
+        {
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                _isLoaded = false;
+                if (loadScene != OWScene.SolarSystem) return;
+
+                PlayerBody playerBody = FindObjectOfType<PlayerBody>();
+
+                GameObject lightGO = new GameObject("DreamDimLight");
+                lightGO.transform.parent = playerBody.transform;
+                lightGO.transform.localPosition = Vector3.zero;
+
+                _light = lightGO.AddComponent<Light>();
+                _light.type = LightType.Point;
+                _light.color = Color.white;
+                _light.shadows = LightShadows.None;
+
+                UpdateDreamDimLightOptions();
+
+                ModHelper.Events.Subscribe<DreamLanternController>(Events.AfterStart);
+                ModHelper.Events.Event += OnEvent;
+            };
+        }
+
+        void Update()
+        {
+            if (!_isLoaded || !_enabled)
+                return;
+
+            _light.gameObject.SetActive(_dreamLanternController.isActiveAndEnabled);
+        }
+        
+        void OnEvent(MonoBehaviour behaviour, Events ev)
+        {
+            if (behaviour is DreamLanternController dlc && ev == Events.AfterStart)
+            {
+                _dreamLanternController = dlc;
+                _isLoaded = true;
+            }
+        }
+
+        void UpdateDreamDimLightOptions()
+        {
+            _light.range = _lightRange;
+            _light.intensity = _lightIntensity;
+
+            if (!_enabled)
+                _light.gameObject.SetActive(false);
+        }
+
+        public override void Configure(IModConfig config)
+        {
+            _lightRange = config.GetSettingsValue<float>("range");
+            _lightIntensity = config.GetSettingsValue<float>("intensity") / 10f;
+            _enabled = config.Enabled;
+
+            if (_light != null)
+                UpdateDreamDimLightOptions();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# Echoes of the Eye: Brighter Dreams mod
+
+:warning: **Potential spoilers ahead**
+
+Gives the player night vision on Echoes of the Eye's dream world.
+
+This mod can be viewed as an accessibility/help option for people with nyctaphobia and for those who find the DLC too difficult or scary.
+
+You can adjust both the `range` and `intensity` of the night vision, so don't hesitate to tweak the settings to best suit your needs.
+
+By default, the settings are left very low to preserve the original experience as much as possible, but it is just enough to see where you walk.
+
+> :warning: **I strongly recommend playing with the default settings to enjoy the almost-vanilla experience.** Increase the settings, *gradually*, only if the game really makes you uncomfortable. Although it was not made for that purpose, this mod can allow you to cheat your way through the DLC if the settings are pushed too far.
+
+## Settings
+From the main/pause menu, go to `Mods` then `EotE Brighter Dreams`:
+
+ - **Night vision range**: how far your vision goes in the dark *(default: 10)*
+ - **Night vision intensity**: how well you can see in the dark *(default: 1)*
+
+![Default settings](Readme/settings.png)
+
+## Settings comparison
+
+![Default settings](Readme/none.png)
+*Without the mod (vanilla)*
+
+![Default settings](Readme/default.png)
+*Mod default settings (subtle night vision, recommended)*
+
+![Default settings](Readme/max.png)
+*Mod max settings*
